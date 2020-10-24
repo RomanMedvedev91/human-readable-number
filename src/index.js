976bd068c97702a63d0cb47684928ef0ba7ec402
Roman Medvedev <roman.medvedev77@gmail.com>
2020-10-24 17:04:36 +0500

    init: initial commit with solution 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-var converter = require("number-to-words-en");
-
 module.exports = function toReadable(number) {
+    var converter = require("number-to-words-en");
     return converter.toWords(number).replace("-", " ");
 };
